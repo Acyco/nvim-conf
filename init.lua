90987db42450180897d459b04445e9622b4a03a4
Acyco <chencongyi@live.com>
2023-10-28 08:32:31 +0800

    Add 'setting.lua' file and the vim setting config of 'init.lua' file move to 'setting.lua'

--- init.lua
+++ init.lua
@@ -1,41 +1,7 @@
 local set = vim.o
 local keymap = vim.keymap
 
-
--- 显示行号
-set.number = true
--- 相对行号
-set.relativenumber = true
--- 剪切板
-set.clipboard = "unnamed"
--- 设置编辑时制表符占用空格数
-set.tabstop = 4
--- 设置格式化时制表符占用空格数
-set.shiftwidth = 4
--- 让vim把连续数量的空格视为一个制表符
-set.softtabstop = 4
--- 允许在有未保存的修改时切换缓冲区，此时的修改由 vim 负责保存
-set.hidden = true
--- 开启自动缩进
-set.autoindent = true
--- 设置使用C/C++语言的自动缩进方式
-set.cindent = true
--- 自动选择对齐方式
-set.smartindent = true
--- 智能的判断要不要区分搜索模式大小写
-set.smartcase = true
--- 文件在vim之外修改过，自动重新读入
-set.autoread = true
--- 支持鼠标
-set.mouse="a"
--- 打开状态栏标尺
-set.ruler = true
--- 把插入的 tab 字符替换成特定数目的空格
-set.expandtab = true
--- 文件编码
-set.encoding="utf-8"
-
-
+require("setting")
 -- 复制高亮 300ms高亮时间
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	pattern = { "*" },
@@ -276,7 +242,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
 		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
 		vim.keymap.set('n', '<space>wl', function()
